d7fa22b62ddb1bf06e43c17b985b6e0ba40b9e3f
Really'if.s <52651839+Reallyifs@users.noreply.github.com>
2019-07-13 12:34:03 +0800

    Update ProDisorderBeam.cs

--- Projectiles/Disorder/ProDisorderBeam.cs
+++ Projectiles/Disorder/ProDisorderBeam.cs
@@ -1,5 +1,6 @@
-﻿using Terraria.ModLoader;
 using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
 using DisorderUnderstar.Utils;
 using Microsoft.Xna.Framework;
 namespace DisorderUnderstar.Projectiles.Disorder
@@ -15,11 +16,14 @@ namespace DisorderUnderstar.Projectiles.Disorder
             projectile.melee = true;
             projectile.scale = 1.5f;
             projectile.width = 18;
+            projectile.damage = 666;
             projectile.height = 18;
             projectile.aiStyle = -1;
             projectile.hostile = false;
             projectile.friendly = true;
             projectile.timeLeft = 1200;
+            projectile.ignoreWater = false;
+            projectile.tileCollide = true;
         }
         public override void AI()
         {
@@ -31,13 +35,46 @@ namespace DisorderUnderstar.Projectiles.Disorder
             {
                 projectile.damage = projectile.timeLeft * 2 + 2333;
                 Dust dust = Dust.NewDustDirect
-                        (projectile.position, projectile.width, projectile.height,
-                        MyDustId.Fire, 0f, 0f, 10);
+                        (projectile.Center, projectile.width + 2, projectile.height + 2,
+                        MyDustId.Fire, -projectile.velocity.X, -projectile.velocity.Y,
+                        128, default(Color), 1.5f);
+                dust.noLight = false;
                 dust.noGravity = true;
             }
             else
             {
                 projectile.damage = projectile.timeLeft * 3 + 666;
+            }
+            if (projectile.timeLeft <= 1197)
+            {
+                NPC tar = null;
+                float disMAX = 1000f;
+                Player pl = Main.player[projectile.owner];
+                foreach(NPC npc in Main.npc)
+                {
+                    if (npc.active && !npc.friendly && npc.type != NPCID.TargetDummy &&
+                        !npc.behindTiles && npc.type != NPCID.LunarTowerNebula &&
+                        npc.type != NPCID.LunarTowerSolar &&
+                        npc.type != NPCID.LunarTowerStardust &&
+                        npc.type != NPCID.LunarTowerVortex)
+                    {
+                        float dis = Vector2.Distance(npc.Center, projectile.Center);
+                        if (disMAX >= dis)
+                        {
+                            tar = npc;
+                            disMAX = dis;
+                        }
+                    }
+                }
+                if (tar != null)
+                {
+                    Vector2 tarVEC = Vector2.Normalize(tar.Center - projectile.Center);
+                    tarVEC *= 20f;
+                    float nVEC = 25f;
+                    if (nVEC <= 25f) nVEC -= 0.1f;
+                    projectile.velocity =
+                        (projectile.velocity * nVEC + tarVEC) / (nVEC + 1f);
+                }
             }
         }
     }

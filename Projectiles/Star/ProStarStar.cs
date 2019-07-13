42e291e0109fad13bb2afa38308e7870d7ac3c45
Really'if.s <52651839+Reallyifs@users.noreply.github.com>
2019-07-13 12:34:47 +0800

    Update ProStarStar.cs

--- Projectiles/Star/ProStarStar.cs
+++ Projectiles/Star/ProStarStar.cs
@@ -1,4 +1,4 @@
-﻿using Terraria;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 using DisorderUnderstar.Utils;
@@ -69,45 +69,42 @@ namespace DisorderUnderstar.Projectiles.Star
                     dust.noLight = false;
                 }
             }
+            if (projectile.timeLeft <= 297)
             {
+                NPC tar = null;
+                float disMAX = 500f;
                 Player player = Main.player[projectile.owner];
-                if (projectile.timeLeft < 297)
+                foreach (NPC npc in Main.npc)
                 {
-                    NPC tar = null;
-                    float disMAX = 500f;
-                    foreach (NPC npc in Main.npc)
+                    // 如果npc活着且敌对
+                    if (npc.active && !npc.friendly && npc.type != NPCID.TargetDummy)
                     {
-                        // 如果npc活着且敌对
-                        if (npc.active && !npc.friendly && npc.type != NPCID.TargetDummy)
+                        // 计算与弹幕的距离
+                        float dis = Vector2.Distance(npc.Center, projectile.Center);
+                        // 如果npc距离比当前最大距离小
+                        if (dis <= disMAX)
                         {
-                            // 计算与玩家的距离
-                            float dis = Vector2.Distance(npc.Center, player.Center);
-                            // 如果npc距离比当前最大距离小
-                            if (dis <= disMAX)
-                            {
-                                // 就把最大距离设置为npc和玩家的距离
-                                // 并且暂时选取这个npc为距离最近npc
-                                disMAX = dis;
-                                tar = npc;
-                            }
+                            // 就把最大距离设置为npc和玩家的距离
+                            // 并且暂时选取这个npc为距离最近npc
+                            disMAX = dis;
+                            tar = npc;
                         }
                     }
-                    // 如果找到符合条件的npc
-                    if (tar != null)
-                    {
-                        // 计算朝向目标的向量
-                        Vector2 tarVEC = tar.Center - projectile.Center;
-                        tarVEC.Normalize();
-                        // 目标向量是朝向目标的大小为20的向量
-                        tarVEC *= 20f;
-                        // 声明一个float变量，并随之减小
-                        float nVEC = 30f;
-                        if (nVEC <= 30f)
-                            nVEC -= 0.1f;
-                        // 朝向npc的单位向量 * nVEC + 3.33%偏移量
-                        projectile.velocity =
-                            (projectile.velocity * nVEC + tarVEC) / (nVEC + 1f);
-                    }
+                }
+                // 如果找到符合条件的npc
+                if (tar != null)
+                {
+                    // 计算朝向目标的向量
+                    Vector2 tarVEC = tar.Center - projectile.Center;
+                    tarVEC.Normalize();
+                    // 目标向量是朝向目标的大小为20的向量
+                    tarVEC *= 20f;
+                    // 声明一个float变量，并随之减小
+                    float nVEC = 30f;
+                    if (nVEC <= 30f) nVEC -= 0.1f;
+                    // 朝向npc的单位向量 * nVEC + 3.33%偏移量
+                    projectile.velocity =
+                        (projectile.velocity * nVEC + tarVEC) / (nVEC + 1f);
                 }
             }
         }
@@ -124,10 +121,10 @@ namespace DisorderUnderstar.Projectiles.Star
         }
         public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
         {
-            target.AddBuff(BuffID.OnFire, 600);
-            target.AddBuff(BuffID.Confused, damage * 5);
             target.AddBuff(BuffID.Venom, 600);
+            target.AddBuff(BuffID.OnFire, 600);
             target.AddBuff(BuffID.Frozen, damage);
+            target.AddBuff(BuffID.Confused, damage * 5);
         }
     }
 }

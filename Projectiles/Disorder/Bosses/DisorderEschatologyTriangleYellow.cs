e15bbbfb427a69748dc620b9aa992783e91c975b
Really'if.s <52651839+Reallyifs@users.noreply.github.com>
2019-07-13 12:35:40 +0800

    Update DisorderEschatologyTriangleYellow.cs

--- Projectiles/Disorder/Bosses/DisorderEschatologyTriangleYellow.cs
+++ Projectiles/Disorder/Bosses/DisorderEschatologyTriangleYellow.cs
@@ -1,4 +1,4 @@
-﻿using Terraria;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 using DisorderUnderstar.Utils;
@@ -10,6 +10,14 @@ namespace DisorderUnderstar.Projectiles.Disorder.Bosses
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("三角型攻击");
+            Main.projFrames[projectile.type] = 6;
+            projectile.frameCounter++;
+            if (projectile.frameCounter >= 10)
+            {
+                projectile.frame++;
+                projectile.frameCounter = 0;
+            }
+            if (projectile.frame >= 6) projectile.frame = 0;
         }
         public override void SetDefaults()
         {
@@ -27,27 +35,56 @@ namespace DisorderUnderstar.Projectiles.Disorder.Bosses
             projectile.penetrate = 1;
             projectile.ignoreWater = false;
             projectile.tileCollide = true;
-            Main.projFrames[projectile.type] = 6;
         }
         public override void AI()
         {
-            Dust dust = Dust.NewDustDirect
-                (projectile.position, projectile.width, projectile.height,
-                MyDustId.YellowFx, 0f, 0f, 100,
-                Color.Yellow, 1f);
-            dust.noGravity = true;
-            dust.noLight = false;
+            if (projectile.timeLeft <= 149)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    Dust dust = Dust.NewDustDirect
+                        (projectile.Center, projectile.width + 2, projectile.height + 2,
+                        MyDustId.YellowFx, 0f, 0f, 100,
+                        Color.Yellow, 1f);
+                    dust.noLight = false;
+                    dust.noGravity = true;
+                }
+                Player pl = null;
+                float disMAX = 300f;
+                foreach (Player player in Main.player)
+                {
+                    if (player.active && player.aggro > 0)
+                    {
+                        float dis = Vector2.Distance(projectile.Center, player.Center);
+                        if (disMAX >= dis)
+                        {
+                            pl = player;
+                            disMAX = dis;
+                        }
+                    }
+                    else if (pl != null)
+                    {
+                        Vector2 plVEC = Vector2.Normalize(pl.Center - projectile.Center);
+                        plVEC *= 35f;
+                        float nVEC = 40f;
+                        if (nVEC >= 40) nVEC -= 0.1f;
+                        projectile.velocity =
+                            (projectile.velocity * nVEC + plVEC) / (nVEC += 1f);
+                    }
+
+                }
+            }
         }
         public override void Kill(int timeLeft)
         {
             for (int i = 0; i < 5; i++)
             {
                 Dust dust = Dust.NewDustDirect
-                    (projectile.position, projectile.width, projectile.height,
-                    MyDustId.YellowFx, 0f, 0f, -10,
-                    Color.Yellow, -0.5f);
-                dust.noGravity = true;
+                    (projectile.Center, projectile.width + 2, projectile.height + 2,
+                    MyDustId.YellowFx, 0f, 0f, 128,
+                    Color.Yellow, 1.5f);
                 dust.noLight = false;
+                dust.noGravity = true;
             }
         }
         public override void OnHitPlayer(Terraria.Player target, int damage, bool crit)

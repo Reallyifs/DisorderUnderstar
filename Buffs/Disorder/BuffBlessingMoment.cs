c049ab77a55c5bd45417b5e9760f6251b188fe75
Really'if.s <52651839+Reallyifs@users.noreply.github.com>
2019-07-20 20:36:21 +0800

    不知道更新了哪里，版本v0.0.2.0-3（2019.7.20）

--- Buffs/Disorder/BuffBlessingMoment.cs
+++ Buffs/Disorder/BuffBlessingMoment.cs
@@ -1,6 +1,8 @@
 ﻿using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using DisorderUnderstar.Dusts.Disorder;
 namespace DisorderUnderstar.Buffs.Disorder
 {
     public class BuffBlessingMoment : ModBuff
@@ -16,42 +18,35 @@ namespace DisorderUnderstar.Buffs.Disorder
         public override void Update(Player player, ref int buffIndex)
         {
             int timer = 0;
-            if(Main.rand.Next(100) < 1)
+            if (Main.rand.Next(100) < 1) timer = 1;
             {
-                timer = 1;
+                #region timer的加减乘除
+                if (timer == 1) timer++;
+                else if (timer >= 2) timer *= 2;
+                else if (timer > 500) timer = 0;
+                #endregion
+                #region player的加减乘除
+                if (timer > 10 && timer <= 100)
+                {
+                    player.statLife += 12;
+                }
+                else if (timer > 100 && timer <= 450)
+                {
+                    player.statLife += 5;
+                    player.AddBuff(BuffID.Daybreak, 1);
+                }
+                else
+                {
+                    player.statLife += 30;
+                    player.AddBuff(BuffID.AmmoBox, 1);
+                    player.AddBuff(BuffID.Daybreak, 1);
+                    player.AddBuff(BuffID.MagicLantern, 1);
+                }
+                #endregion
             }
-            {
-                if (timer == 1)
-                {
-                    timer++;
-                }
-                else if (timer >= 2)
-                {
-                    timer *= 2;
-                }
-                else if (timer > 500)
-                {
-                    timer = 0;
-                }
-                {
-                    if (timer > 10 && timer < 100)
-                    {
-                        player.statLife += 12;
-                    }
-                    else if (timer > 100 && timer < 450)
-                    {
-                        player.statLife += 5;
-                        player.AddBuff(BuffID.Daybreak, 1);
-                    }
-                    else
-                    {
-                        player.statLife += 30;
-                        player.AddBuff(BuffID.AmmoBox, 1);
-                        player.AddBuff(BuffID.Daybreak, 1);
-                        player.AddBuff(BuffID.MagicLantern, 1);
-                    }
-                }
-            }
+            Vector2 pleft = new Vector2(player.Center.X - player.width, player.Center.Y);
+            Dust.NewDustDirect(pleft - Main.screenPosition, player.width, player.height,
+                mod.DustType("DustGoldenSymbol"), player.velocity.X, player.velocity.Y);
         }
     }
 }
--- Dusts/Disorder/DustGoldenSymbol.cs
+++ Dusts/Disorder/DustGoldenSymbol.cs
@@ -6,13 +6,54 @@ namespace DisorderUnderstar.Dusts.Disorder
 {
     public class DustGoldenSymbol : ModDust
     {
+        private readonly Player pl = Main.player[Main.myPlayer];
         public override void OnSpawn(Dust dust)
         {
             dust.alpha = 25;
             dust.color = Color.Gold;
             dust.scale = 0.9f;
+            dust.fadeIn = 0f;
+            dust.noLight = false;
             dust.noGravity = false;
-            Main.RegisterItemAnimation(dust.type, new DrawAnimationVertical(8, 60));
+            dust.velocity *= pl.velocity.X + pl.moveSpeed;
+            dust.velocity *= pl.velocity.Y;
+        }
+        public override bool Update(Dust dust)
+        {
+            Main.maxDustToDraw = pl.whoAmI;
+            dust.fadeIn++;
+            #region dust的位移
+            if (dust.position.Y < pl.position.Y) dust.velocity.Y += 0.1f;
+            else if (dust.position.Y == pl.velocity.Y) dust.velocity.Y = 0;
+            else dust.position.Y -= 0.1f;
+            if (dust.position.X < pl.position.X) dust.velocity.X += 0.1f;
+            else if (dust.position.X == pl.velocity.X) dust.velocity.X = 0;
+            else dust.position.X -= 0.1f;
+            #endregion
+            #region dust的消失
+            if (dust.active && dust.fadeIn < 3f)
+            {
+                Lighting.AddLight((int)(dust.position.X / 16f), (int)(dust.position.Y / 16f),
+                    0.5f, 0.5f, 0f);
+                dust.alpha = 100;
+                dust.color = Color.Lerp(new Color(200, 200, 20), Color.Yellow, (dust.fadeIn / 5f));
+                dust.scale = 1.5f;
+                dust.rotation += 0.1f;
+            }
+            else if (dust.fadeIn < 8f)
+            {
+                dust.color = Color.Lerp(Color.Yellow, Color.Gold, (dust.fadeIn - 5f) / 5f);
+                dust.scale -= 0.125f;
+                dust.rotation += 0.1f;
+            }
+            else
+            {
+                dust.scale -= 0.1f;
+                dust.rotation += 0.1f;
+            }
+            if (dust.scale == 0.4f) dust.active = false;
+            #endregion
+            return false;
         }
     }
 }

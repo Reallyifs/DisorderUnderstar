72339aafbe97bd21ab89c46cd4a3ea8a96730407
Really'if.s <52651839+Reallyifs@users.noreply.github.com>
2019-09-01 15:15:41 +0800

    增加DustCodeParticle一句

---
+++ Dusts/Code/DustCodeParticle/DustCodeParticle3.cs
@@ -0,0 +1,26 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+namespace DisorderUnderstar.Dusts.Code.DustCodeParticle
+{
+    public class DustCodeParticle3 : ModDust
+    {
+        public override void OnSpawn(Dust dust)
+        {
+            dust.alpha = 51;
+            dust.color = new Color(000, 128, 000);
+            dust.noLight = false;
+            dust.dustIndex = 13;
+            dust.noGravity = true;
+        }
+        public override bool Update(Dust dust)
+        {
+            #region 生成下一个Dust
+            if (dust.dustIndex <= 1)
+                Dust.NewDustDirect(dust.position, 1, 1, mod.DustType("DustCodeParticle4"),
+                    0, 0, 102, Color.Green, 1f);
+            #endregion
+            return false;
+        }
+    }
+}
---
+++ Dusts/Code/DustCodeParticle/DustCodeParticle4.cs
@@ -0,0 +1,17 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+namespace DisorderUnderstar.Dusts.Code.DustCodeParticle
+{
+    public class DustCodeParticle4 : ModDust
+    {
+        public override void OnSpawn(Dust dust)
+        {
+            dust.alpha = 51;
+            dust.color = new Color(000, 128, 000);
+            dust.noLight = false;
+            dust.dustIndex = 13;
+            dust.noGravity = true;
+        }
+    }
+}

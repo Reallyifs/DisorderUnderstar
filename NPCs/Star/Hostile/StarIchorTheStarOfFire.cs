81ae81e9fcd3b5b4035c7c459f53ea18b6b64be7
Really'if.s <52651839+Reallyifs@users.noreply.github.com>
2019-09-01 15:34:09 +0800

    更新语句，版本v0.0.2.0（2019.9.1）

--- NPCs/Star/Hostile/StarIchorTheStarOfFire.cs
+++ NPCs/Star/Hostile/StarIchorTheStarOfFire.cs
@@ -4,29 +4,24 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using DisorderUnderstar.Utils;
 using Microsoft.Xna.Framework;
+using DisorderUnderstar.Items.Star;
 namespace DisorderUnderstar.NPCs.Star.Hostile
 {
-    public class StarIchorTheStarOfFire : DUNPC
+    public class StarIchorTheStarOfFire : ModNPC
     {
-        enum NPCState
-        {
-            Normal,
-            NAttack,
-            YAttack,
-            LAttack
-        }
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("灵液火之星");
         }
         public override void SetDefaults()
         {
+            aiType = NPCID.EyeofCthulhu;
             animationType = NPCID.MeteorHead;
-            npc.damage = 32;
-            npc.defense = 12;
+            npc.damage = 40;
+            npc.defense = 15;
             npc.lifeMax = 2400;
             npc.friendly = false;
-            npc.lifeRegen += 9;
+            npc.lifeRegen = 9;
             npc.noGravity = true;
             npc.noTileCollide = true;
             npc.knockBackResist = 0.1f;
@@ -34,126 +29,13 @@ namespace DisorderUnderstar.NPCs.Star.Hostile
             npc.buffImmune[BuffID.OnFire] = true;
             npc.buffImmune[BuffID.Confused] = true;
             banner = npc.type;
-            bannerItem = mod.ItemType("FireOfStarZero");
-            Main.npcFrameCount[npc.type] = 2;
-        }
-        public override void AI()
-        {
-            switch ((NPCState)State)
-            {
-                case NPCState.Normal:
-                    {
-                        npc.color = Color.Yellow;
-                        npc.defense = 210;
-                        for (int i = 0; i < 1; i++)
-                        {
-                            Dust.NewDustDirect
-                                (npc.position, npc.width, npc.height,
-                                MyDustId.YellowFx, 0f, 0f, +85,
-                                Color.Yellow, -0.3f);
-                        }
-                        break;
-                    }
-                case NPCState.NAttack:
-                    {
-                        aiType = NPCID.MeteorHead;
-                        npc.color = Color.Yellow;
-                        npc.defense = 12;
-                        for (int i = 0; i < 1; i++)
-                        {
-                            Dust.NewDustDirect
-                                (npc.position, npc.width, npc.height,
-                                MyDustId.YellowFx, 0f, 0f, +85,
-                                Color.Yellow, -0.3f);
-                            Player p = Main.player[npc.target];
-                            Projectile.NewProjectile
-                                (p.position, p.velocity, mod.ProjectileType("ProStarITSOF1"),
-                                npc.damage + npc.life, 2f, npc.damage);
-                        }
-                        break;
-                    }
-                case NPCState.YAttack:
-                    {
-                        aiType = NPCID.MeteorHead;
-                        npc.color = Color.Lime;
-                        npc.damage += 28;
-                        npc.defense += 34;
-                        for (int i = 0; i < 1; i++)
-                        {
-                            Dust.NewDustDirect
-                                (npc.position, npc.width, npc.height,
-                                MyDustId.YellowFx, 0f, 0f, +85,
-                                Color.Yellow, -0.3f);
-                            Player p = Main.player[npc.target];
-                            Projectile.NewProjectile
-                                (p.position, p.velocity, mod.ProjectileType("ProStarITSOF2"),
-                                npc.damage + p.dpsDamage, 2.5f, npc.damage + 10);
-                        }
-                        break;
-                    }
-                case NPCState.LAttack:
-                    {
-                        aiType = NPCID.EyeofCthulhu;
-                        npc.damage += 16;
-                        npc.defense += 7;
-                        npc.behindTiles = true;
-                        for (int i = 0; i < 2; i++)
-                        {
-                            Dust.NewDustDirect
-                                (npc.position, npc.width, npc.height,
-                                MyDustId.YellowFx, 0f, 0f, +128,
-                                Color.Lime, -0.5f);
-                        }
-                        if (Main.rand.Next(10) < 3)
-                        {
-                            for (int i = 0; i < 3; i++)
-                            {
-                                Player p = Main.player[npc.target];
-                                Projectile.NewProjectile
-                                    (p.position, p.velocity, mod.ProjectileType("ProStarITSOF3"),
-                                    npc.damage + p.dpsDamage + 10, 3.5f, npc.damage + 20);
-                            }
-                        }
-                        break;
-                    }
-                default:
-                    {
-                        npc.damage = 64;
-                        npc.defense = 24;
-                        npc.lifeMax = 4800;
-                        npc.lifeRegen += 18;
-                        npc.knockBackResist = 0.05f;
-                        for(int i = 0; i < 2; i++)
-                        {
-                            Dust.NewDustDirect
-                                    (npc.position, npc.width, npc.height,
-                                    MyDustId.GreenFXPowder, 0f, 0f, +128,
-                                    Color.LimeGreen, -0.5f);
-                        }
-                        {
-                            if (Main.player.Any
-                                (p => p.active && p.Distance(npc.Center) < 600))
-                            {
-                                SwitchState((int)NPCState.NAttack);
-                            }
-                            else if (npc.life <= 480)
-                            {
-                                SwitchState((int)NPCState.LAttack);
-                            }
-                            else if (npc.life <= 1200)
-                            {
-                                SwitchState((int)NPCState.YAttack);
-                            }
-                        }
-                    }
-                    break;
-            }
+            bannerItem = mod.ItemType<FireOfStarZero>();
         }
         public override void OnHitPlayer(Player target, int damage, bool crit)
         {
-            target.AddBuff(BuffID.Ichor, 6000 / damage);
             target.AddBuff(BuffID.OnFire, damage * 2);
-            target.AddBuff(BuffID.BrokenArmor, Player.tileRangeX / Player.tileRangeY);
+            target.AddBuff(BuffID.Ichor, 6000 / damage);
+            target.AddBuff(BuffID.BrokenArmor, (target.statLife * target.statDefense) / target.statMana);
         }
         public override float SpawnChance(NPCSpawnInfo spawnInfo)
         {
@@ -163,58 +45,28 @@ namespace DisorderUnderstar.NPCs.Star.Hostile
         }
         public override void NPCLoot()
         {
-            Item.NewItem((int)npc.velocity.X, (int)npc.velocity.Y,
-                npc.width, npc.height, ItemID.SilverCoin, 31);
-            Item.NewItem((int)npc.velocity.X, (int)npc.velocity.Y,
-                npc.width, npc.height, ItemID.CopperCoin, 72);
-            if (Main.rand.Next(100) < 1)
-            {
-                Item.NewItem((int)npc.velocity.X, (int)npc.velocity.Y,
-                    npc.width, npc.height, ItemID.DepthMeter);
-            }
-            else if (Main.rand.Next(99) < 1)
-            {
-                Item.NewItem((int)npc.velocity.X, (int)npc.velocity.Y,
-                    npc.width, npc.height, ItemID.Compass);
-            }
-            if (Main.rand.Next(5) < 1)
-            {
-                Item.NewItem((int)npc.velocity.X, (int)npc.velocity.Y,
-                    npc.width, npc.height, mod.ItemType("FireOfStarZero"), 1);
-            }
-            else if (Main.rand.Next(10) < 1)
-            {
-                Item.NewItem((int)npc.velocity.X, (int)npc.velocity.Y,
-                    npc.width, npc.height, mod.ItemType("FireOfStarZero"), 2);
-            }
-            else if (Main.rand.Next(20) < 1)
-            {
-                Item.NewItem((int)npc.velocity.X, (int)npc.velocity.Y,
-                    npc.width, npc.height, mod.ItemType("FireOfStarZero"), 3);
-            }
+            #region 掉落
+            Item.NewItem((int)npc.velocity.X, (int)npc.velocity.Y, npc.width, npc.height, ItemID.SilverCoin, 31);
+            Item.NewItem((int)npc.velocity.X, (int)npc.velocity.Y, npc.width, npc.height, ItemID.CopperCoin, 72);
+            if (Main.rand.Next(100) < 1) Item.NewItem((int)npc.velocity.X, (int)npc.velocity.Y, npc.width, npc.height, ItemID.DepthMeter);
+            else if (Main.rand.Next(99) < 1) Item.NewItem((int)npc.velocity.X, (int)npc.velocity.Y, npc.width, npc.height, ItemID.Compass);
+            if (Main.rand.Next(5) < 1) Item.NewItem((int)npc.velocity.X, (int)npc.velocity.Y, npc.width, npc.height,
+                mod.ItemType<FireOfStarZero>(), 1);
+            else if (Main.rand.Next(10) < 1) Item.NewItem((int)npc.velocity.X, (int)npc.velocity.Y, npc.width, npc.height,
+                mod.ItemType<FireOfStarZero>(), 2);
+            else if (Main.rand.Next(20) < 1) Item.NewItem((int)npc.velocity.X, (int)npc.velocity.Y, npc.width, npc.height,
+                mod.ItemType<FireOfStarZero>(), 3);
             if (Main.hardMode)
             {
-                if (Main.rand.Next(15) < 1)
-                {
-                    Item.NewItem((int)npc.velocity.X, (int)npc.velocity.Y,
-                        npc.width, npc.height, mod.ItemType("FireOfStarZero"), 3);
-                }
-                else if (Main.rand.Next(5) < 1)
-                {
-                    Item.NewItem((int)npc.velocity.X, (int)npc.velocity.Y,
-                        npc.width, npc.height, mod.ItemType("FireOfStarZero"), 2);
-                }
-                else if (Main.rand.Next(1) < 1)
-                {
-                    Item.NewItem((int)npc.velocity.X, (int)npc.velocity.Y,
-                        npc.width, npc.height, mod.ItemType("FireOfStarZero"), 1);
-                }
-                if (Main.rand.Next(4) < 1)
-                {
-                    Item.NewItem((int)npc.velocity.X, (int)npc.velocity.Y,
-                        npc.width, npc.height, ItemID.Ichor, 2);
-                }
+                if (Main.rand.Next(15) < 1) Item.NewItem((int)npc.velocity.X, (int)npc.velocity.Y, npc.width, npc.height,
+                    mod.ItemType<FireOfStarZero>(), 3);
+                else if (Main.rand.Next(5) < 1) Item.NewItem((int)npc.velocity.X, (int)npc.velocity.Y, npc.width, npc.height,
+                    mod.ItemType<FireOfStarZero>(), 2);
+                else if (Main.rand.Next(1) < 1) Item.NewItem((int)npc.velocity.X, (int)npc.velocity.Y, npc.width, npc.height,
+                    mod.ItemType<FireOfStarZero>(), 1);
+                if (Main.rand.Next(4) < 1) Item.NewItem((int)npc.velocity.X, (int)npc.velocity.Y, npc.width, npc.height, ItemID.Ichor, 2);
             }
+            #endregion
         }
     }
 }
